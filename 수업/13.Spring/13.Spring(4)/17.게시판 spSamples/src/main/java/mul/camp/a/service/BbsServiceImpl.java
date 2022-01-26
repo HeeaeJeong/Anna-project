b8af18e326cfadc54620837f1015db234c4cda08
anna <sujin6588@gmail.com>
2022-01-26 02:36:18 -0800

    중복 삭제

---
+++ 수업/13.Spring/13.Spring(4)/17.게시판 spSamples/src/main/java/mul/camp/a/service/BbsServiceImpl.java
@@ -0,0 +1,23 @@
+package mul.camp.a.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mul.camp.a.dao.BbsDao;
+import mul.camp.a.dto.BbsDto;
+
+@Service
+public class BbsServiceImpl implements BbsService{
+
+	@Autowired
+	BbsDao dao;
+
+	@Override
+	public List<BbsDto> bbslist() {		
+		return dao.bbslist();
+	}
+	
+	
+}
---
+++ 수업/14.웹 프로젝트/과제(도서 프로그램)/java11/src/main/java/com/scsa/java/fifth/Magazine/Magazine.java
@@ -0,0 +1,103 @@
+package com.scsa.java.fifth.Magazine;
+
+public class Magazine {
+	
+	private String isbn;
+	private String title;
+	private String author;
+	private String publisher;
+	private int year;
+	private int month;
+	private int price;
+	private String desc;
+	
+
+	public Magazine(String isbn, String title, String author, String publisher, int year, int month, int price,
+			String desc) {
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.year = year;
+		this.month = month;
+		this.price = price;
+		this.desc = desc;
+
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return "" + isbn + "\t|" + title + "\t|" + author + "\t|" + publisher
+				+ "\t|" + year +"\t|"+ month + "." + price + "" + desc + "";
+	}
+	
+	
+	
+	
+
+
+}
